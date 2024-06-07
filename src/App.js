50b36c1a6feab162e385dd6b05647b524a809a4f
nord <adam.w.nord@gmail.com>
2024-06-07 11:52:46 -0700

    stylized, and added a scoreboard ability to show the top 5 scores after a round is finished

--- src/App.js
+++ src/App.js
@@ -7,29 +7,73 @@ function App() {
   const [wordList, setWordList] = useState("");
   const [isLoading, setIsLoading] = useState(true);
   const [guessed, setGuessed] = useState("");
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const [timerInterval, setTimerInterval] = useState(null);
+  const [leaderboards, setLeaderboards] = useState([]);
   const wordURL = "https://random-word-api.vercel.app/api?words=1";//"https://random-word-api.herokuapp.com/word"; 
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(wordURL);
       const data = await response.json();
-      setWordList(data);
+      if(wordList === ""){
+        setWordList(data);
+      }
       setIsLoading(false);
+      setTimerStarted(false);
     }
     fetchData();
-  }, [])
+  }, [isLoading])
 
-  function checkValidity(input) {
+
+  const startTimer = () => {
+    setTimerStarted(true);
+    const interValid = setInterval(() => {
+      setElapsedTime(prev => prev + 1);
+    }, 1000);
+    setTimerInterval(interValid);
+  }
+  let a = 0
+  const stopTimer = () => {
+    if( a === 0){
+      a += 1;
+    // setLeaderboards(prev => [...prev, ]);
+    clearInterval(timerInterval);
+    setTimerInterval(null);
+    }
+    //setTimerStarted(false);
+  }
+
+  useEffect(() => {
+    if(!timerInterval && a === 1){
+      setLeaderboards(prev => [...prev, elapsedTime]);
+    }
+  }, [timerInterval, a, elapsedTime])
+
+  const checkValidity = (input) => {
     const test = input.toString();
     if (test === "") {
       alert("PLease type in a letter, or guess the full word!");
     } else if (test.length > 1) {
       alert("Please only guess 1 letter at a time")
     } else {
-      setGuessed(document.getElementById("inputText").value);
+      setGuessed(test);
       document.getElementById("inputText").value = "";
+      if(!timerStarted){
+        startTimer();
+      }
     }
+  }
 
+  const reset = () => {
+    stopTimer();
+    setElapsedTime(0);
+    setWordList("");
+    setIsLoading(true);
+    setGuessed("");
+
+    //window.location.reload();
   }
 
   return (
@@ -42,12 +86,16 @@ function App() {
           <div id="inputFields">
             <label for="inputText">Type in a Letter </label>
             <input id="inputText" type="text"></input>
-            <button type="submit" onClick={() => { checkValidity(document.getElementById("inputText").value); }}>Guess!</button>
+            <button type="submit" onClick={() => { checkValidity(document.getElementById("inputText").value);}}>Guess!</button>
           </div>
+          </div>
+           <div id="timer">
+            {timerStarted && <p>Elapsed Time: {elapsedTime} seconds</p>}
           </div>
           <div id="playSpace">
-            <Game setWord={wordList} setWordList={setWordList} input={guessed} />
+            <Game setWord={wordList} setWordList={setWordList} input={guessed} timer={stopTimer} reset={reset} elapsedTime={elapsedTime} leaderBoards={leaderboards}/>
           </div>
+         
         </div>
       ) : (
         <div>Loadiing...</div>
--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,9 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import CharacterBoxes from "./CharacterBoxes";
 import Confetti from "react-confetti"
 import "./Game.css"
 
-const Game = ({setWord, setWordList, input}) => {
+const Game = ({ setWord, setWordList, input, timer, reset, leaderBoards, elapsedTime }) => {
 
     const word = setWord.toString().toUpperCase();
     let charArrayOfWord = word.split('');
@@ -12,55 +12,96 @@ const Game = ({setWord, setWordList, input}) => {
     const [indexes, setIndexes] = useState([]);
     const dictionary = "https://api.dictionaryapi.dev/api/v2/entries/en/";
     const [wordDef, setWordDef] = useState("");
-    
+    const [wordType, setWordType] = useState("");
+    const [confetti, setConfetti] = useState(false);
+    const [showLeaderboard, setShowLeaderboard] = useState(false);
+    const [top5, setTop5] = useState([]);
+
     useEffect(() => {
-        if(inputLetter !== null){
-        const newIndexes = [];
-        for(let i = 0; i < charArrayOfWord.length; i++){
-            if(charArrayOfWord[i] === inputLetter){
-                newIndexes.push(i);
+        if (inputLetter !== null) {
+            const newIndexes = [];
+            for (let i = 0; i < charArrayOfWord.length; i++) {
+                if (charArrayOfWord[i] === inputLetter) {
+                    newIndexes.push(i);
+                }
             }
-        }
-        setGuesses(prev => [...prev, ...input])
-        setIndexes(prev => [...prev, ...newIndexes]);
+            setGuesses(prev => [...prev, ...input])
+            setIndexes(prev => [...prev, ...newIndexes]);
         }
     }, [input]);
 
     useEffect(() => {
-        const fetchData = async() => {
-            let response
-            try{
-                response = await fetch(dictionary + word);
-                if(!response.ok){
-                    throw new Error(`Invalid URL: ${word} not in this dictionary`)
-                }
-                const data = await response.json();
-                setWordDef(data[0].meanings[0].definitions[0].definition);
-            } catch(error){
-                if(!response.ok)
-                console.error(error.message);
-            }
+        if(indexes.length === charArrayOfWord.length && !confetti){
+            timer();
+            fetch(dictionary + word)
+                .then(response => response.json())
+                .then(data => {
+                    setWordDef(data[0].meanings[0].definitions[0].definition);
+                    setWordType(data[0].meanings[0].partOfSpeech);
+                })
+                .catch(error => {
+                    console.error("Error fetching data:", error);
+                });
+                const board = leaderBoards.sort();
+                    setTop5(board.slice(0,5));
+                    let temp = board.slice(0,5);
+                    let x = temp.indexOf(parseInt(elapsedTime));
+                    if(x !== -1 && x <= 4){
+                        setConfetti(true);
+                    }
+                    setShowLeaderboard(true);
         }
-        fetchData();
-    }, [setWord]);
-
-    function reset(){
-        window.location.reload();
-    }
-
+    }, [indexes, charArrayOfWord.length, confetti, leaderBoards, timer, elapsedTime, dictionary, setWord, word]);
     
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         let response
+    //         try {
+    //             response = await fetch(dictionary + word);
+    //             if (!response.ok) {
+    //                 throw new Error(`Invalid URL: ${word} not in this dictionary`)
+    //             }
+    //             const data = await response.json();
+    //             setWordDef(data[0].meanings[0].definitions[0].definition);
+    //             setWordType(data[0].meanings[0].partOfSpeech);
+    //         } catch (error) {
+    //             if (!response.ok)
+    //                 console.error(error.message);
+    //         }
+    //     }
+    //     fetchData();
+    // }, [setWord]);
 
-    return(
+    // // function reset() {
+    // //     window.location.reload();
+    // // }
+    // useEffect(() => {
+    //     if(indexes.length === charArrayOfWord.length && !confetti){
+    //         timer();
+    //         board = leaderBoards.sort();
+    //         top5 = board.slice(0,5);
+    //         setShowLeaderboard(true);
+    //         if(top5.includes(parseInt(elapsedTime))){
+    //             setConfetti(true);
+    //         }
+    //     }
+    // }, [indexes, charArrayOfWord.length, confetti, leaderBoards, timer, elapsedTime])
+    return (
         <>
 
             <div id="board">
-                {indexes.length === charArrayOfWord.length && <button class="button-53" onClick={reset}>Play Again!</button> }
-                <p id="guesses">{guesses.length !== 0 ? (guesses.map(x => x) + ", "): null}</p>
-                
-            <div id="boxes"><CharacterBoxes setWord={charArrayOfWord} indexes={indexes}/></div>
-            {indexes.length === charArrayOfWord.length?(
-             <div id="definition"><Confetti/><p>{wordDef.length? wordDef : "Hmmm, I need a better dictionary..."}</p></div>
-            ):(null)}
+                {indexes.length === charArrayOfWord.length && <button class="button-53" onClick={reset}>Play Again!</button>}
+                <p id="guesses">{guesses.length !== 0 ? (guesses.map(x => x) + ", ") : null}</p>
+
+                <div id="boxes"><CharacterBoxes setWord={charArrayOfWord} indexes={indexes} /></div>
+                {indexes.length === charArrayOfWord.length ? (
+                    <div id="definition">{confetti? <Confetti/> : null}
+                    <p>{wordDef.length ? `(${wordType}) ${wordDef}` : "Hmmm, I need a better dictionary..."}</p>
+                    <ol id="list">
+                        {showLeaderboard && top5.map(x => <li>  {x} seconds</li>)}
+                    </ol>
+                    </div>
+                ) : (null)}
             </div>
         </>
     );
